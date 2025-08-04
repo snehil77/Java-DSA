800ab8fbdb2f4ac92297ea385e9635c0178731b3
snehil77 <snehil142@gmail.com>
2025-08-04 22:13:09 +0530

    all files are added

---
+++ Lecture4Functions/Exercise1.java
@@ -0,0 +1,66 @@
+package Lecture4Functions;
+import java.util.*;
+public class Exercise1 {
+    public static void main(String []args)
+    {
+    //  **** q1-> Enter 3 numbers from the user & make a function to print their average
+     Scanner sc=new Scanner(System.in);
+    //  System.out.println("Enter number 1");
+    //  int a=sc.nextInt();
+    //  System.out.println("Enter number 2");
+    //  int b=sc.nextInt();
+    //  System.out.println("Enter number 3");
+    //  int c=sc.nextInt();
+
+    //  average(a, b, c);
+
+
+
+    // ***q2-> write a function to print the sum of all odd numbers from 1 to n.
+
+    System.out.println("Enter any number");
+    int n=sc.nextInt();
+    sum(n);
+
+
+
+
+
+
+    }
+
+    // **1 
+    // public static void average(int a,int b,int c)
+    // {
+    //     int avr=(a+b+c)/(3);
+    //     System.out.println(avr);
+    // }
+  
+
+
+    // **2 
+    public static void sum(int n)
+    {
+       int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+            {
+                 
+                 sum=sum+i;
+
+            }
+
+            else{
+                
+                System.out.println();
+            }
+
+        }
+
+        System.out.println(sum);
+         
+
+    }
+
+}
